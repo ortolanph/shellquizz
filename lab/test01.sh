d55bf503c842d393bb5c8a4c052818bd109d5320
Paulo Ortolan <paulo.ortolan@gmail.com>
2014-09-23 09:25:43 -0300

    Start commit

---
+++ lab/test01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+zenity --list \
+       --title="Em que ano estreiou Star Wars?" \
+       --column="Alternativa" \
+       "1977" \
+       "1978" \
+       "1976" \
+       "1981" \
+       "2003"
